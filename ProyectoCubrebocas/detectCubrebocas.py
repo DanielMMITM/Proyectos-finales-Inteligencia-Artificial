37d7f4c7e819f72ba97e4f2518d2e8ac3672d920
Edgar Martínez <edgar.danmm@gmail.com>
2023-12-08 20:34:26 -0600

    Cambios en el codigo de detección de cubrebocas y se agrego la gráfica de phaser y la red convolusionar para las flores

--- ProyectoCubrebocas/detectCubrebocas.py
+++ ProyectoCubrebocas/detectCubrebocas.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 
-rostro = cv.CascadeClassifier('C:\\Users\\Edani\\Downloads\\CubrebocasDataset\\Face Mask Dataset\\Train\\classifier\\cascade.xml')
+rostro = cv.CascadeClassifier('C:\\Users\\Edani\\Downloads\\cascade\\classifier\\cascade.xml')
+rostro2 = cv.CascadeClassifier('C:\\Users\\Edani\\Downloads\\CubrebocasDataset\\Face Mask Dataset\\Train\\classifier\\cascade.xml')
 cap = cv.VideoCapture(0)
 
 
@@ -8,12 +9,16 @@ while True:
     ret, frame = cap.read()
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # 1k images
     #scalefactor = 1.01, minNeighbors = 6 o 10 //// 1.1 y 4 C:\\Users\\Edani\\OneDrive\\Documentos\\IA\\Caras\\classifier\\cascade.xml
-    rostros = rostro.detectMultiScale(gray, scaleFactor= 1.41, minNeighbors= 20, minSize=(70, 70))
+    # rostros = rostro.detectMultiScale(gray, scaleFactor= 1.4, minNeighbors= 18, minSize=(70, 70))
     #scalefactor = 1.24, minNeighbors = 50 / 35
-    for (x, y, w, h) in rostros:
+    
+    rostros2 = rostro.detectMultiScale(gray, scaleFactor= 1.32, minNeighbors= 25, minSize=(70, 70))
+    
+    for (x, y, w, h) in rostros2:
         frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv.putText(frame, 'Persona con cubrebocas', (x, y), 2, 0.7, (0, 255, 0), 2, cv.LINE_AA)
+        cv.putText(frame, 'Cubrebocas detectado', (x, y), 2, 0.7, (0, 255, 0), 2, cv.LINE_AA)
 
     cv.imshow('Rostro con cubrebocas', frame)
 
--- ProyectoFlores/generarDatasetFlores.py
+++ ProyectoFlores/generarDatasetFlores.py
@@ -1,19 +1,23 @@
 import cv2 as cv
 from time import sleep
-cap = cv.VideoCapture('C:\\Users\\Edani\\Downloads\\girasol\\girasol2.mp4')
-i=686
+cap = cv.VideoCapture('C:\\Users\\Edani\\Downloads\\nochebuena\\nochebuena2.mp4')
+i=2875
 while cap.isOpened():
     ret, im = cap.read()
     
     if ret==False:
         break
     
+    # Obtén las dimensiones de la imagen
     height, width, _ = im.shape
+    #print(im.shape)
 
-    x1 = int(width / 2) - 200  
-    y1 = int(height / 2) - 360  #arriba
-    x2 = int(width / 2) + 200
-    y2 = int(height / 2) + 70 
+    # Calcula las coordenadas del rectángulo para que esté en el centro
+    x1 = int(width / 2) - 190  #mas alto mas a la izquierda
+    x2 = int(width / 2) + 220  #mas alto mas a la derecha 
+    
+    y1 = int(height / 2) - 200 #mas alto mas arriba 
+    y2 = int(height / 2) + 200 #mas alto mas abajo 
     
     im = cv.rectangle(im, (x1,y1), (x2,y2), (0,255,0), 2)
     cuadro = im[y1:y2, x1:x2]
@@ -25,9 +29,7 @@ while cap.isOpened():
     k=cv.waitKey(1)
     if k == ord('s'):
         i=i+1
-        cv.imwrite('C:\\Users\\Edani\\Downloads\\datasetGirasol\\' + str(i)+'.jpg', mini)
-        if (i == 2833 or i == 4400):
-            print(i)
+        cv.imwrite('C:\\Users\\Edani\\Downloads\\DatasetFlowers\\Nochebuena\\' + str(i)+'.jpg', mini)
     if k == 27:
         break
     sleep(1/25)
